b03efb86c3030ea9b7a3eca5eb7f8cecd0425d01
meleiromariana <minegalaxy24@gmail.com>
2025-04-23 10:45:35 -0300

    (23/04/2025) Aula 9 - Herança e Polimorfismo

---
+++ Aula 9 - Herança e Polimorfismo/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module HerançaPolimorfismo {
+}
---
+++ Aula 9 - Herança e Polimorfismo/src/principal/Main.java
@@ -0,0 +1,21 @@
+package principal;
+
+import entidades.Carro;
+import entidades.Moto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Carro civic = new Carro("Honda", "Civic", 2024, 250, true);
+		System.out.println(civic.toString());
+		System.out.println("O valor do aluguel desse carro foi de R$ " + civic.calcularAluguel(5));
+		System.out.println();
+		
+		Moto xj = new Moto("Yamaha", "XJ6", 2025, 125, 600);
+		System.out.println(xj.toString());
+		System.out.println("O valor do aluguel dessa moto foi de R$ " + xj.calcularAluguel(5));
+	}
+
+
+}
